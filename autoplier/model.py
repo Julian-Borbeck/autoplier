c153d3eb670e390163f7d0e65e77dbf9826e5313
julian-borbeck <julian.borbeck@gmail.com>
2021-11-17 16:42:19 +0100

    code cleanup

--- autoplier/model.py
+++ autoplier/model.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from tensorflow.random import set_seed
 from tensorflow.math import reduce_max, reduce_sum, square
-from tensorflow.keras.layers import Input, Dense, Dropout, ReLU
+from tensorflow.keras.layers import Input, Dense, ReLU
 from tensorflow.keras.initializers import Constant
 from tensorflow.keras.regularizers import l1
 from tensorflow.keras.models import Model
@@ -64,18 +64,17 @@ class autoPLIER:
         self.model.compile(optimizer='adam', loss='mse')
 
     # - - - - - - Model Training  - - - - - -
-    def fit(self, x_train, pathways, callbacks =[], batch_size=50, maxepoch=2000, verbose=2, valfrac=.3):
+    def fit(self, x_train, pathways, callbacks=[], batch_size=50, maxepoch=2000, verbose=2, valfrac=.3):
 
-        x_train_processed = self.preprocess(x_train, pathways, fit = True)
+        x_train_processed = self.preprocess(x_train, pathways, fit=True)
 
         # fit the autoencoder model to reconstruct input
-        history = self.model.fit(x_train_processed, x_train_processed, epochs=maxepoch, batch_size=batch_size, verbose=verbose,
-                                 validation_split=valfrac, callbacks=callbacks)
-
+        history = self.model.fit(x_train_processed, x_train_processed, epochs=maxepoch, batch_size=batch_size,
+                                 verbose=verbose, validation_split=valfrac, callbacks=callbacks)
 
         self.build_encoder()
         comp_dec = self.final_encoder.get_layer('ulayer')
-        self.components_decomposition_ = pd.DataFrame(comp_dec.get_weights()[0], index= pathways.index)
+        self.components_decomposition_ = pd.DataFrame(comp_dec.get_weights()[0], index=pathways.index)
 
         return history
 
@@ -90,7 +89,7 @@ class autoPLIER:
 
     def transform(self, x_predict, pathways):
 
-        x_predict_processed = self.preprocess(x_predict, pathways, fit = False)
+        x_predict_processed = self.preprocess(x_predict, pathways, fit=False)
 
         z_predicted = pd.DataFrame(self.final_encoder.predict(x_predict_processed), index=x_predict.index)
 
@@ -99,10 +98,10 @@ class autoPLIER:
     def fit_transform(self, x_train, pathways, callbacks=[], batch_size=50, maxepoch=2000, verbose=2, valfrac=.3):
         # fit the autoencoder model to reconstruct input
 
-        x_train_processed = self.preprocess(x_train, pathways, fit = True)
+        x_train_processed = self.preprocess(x_train, pathways, fit=True)
 
         self.model.fit(x_train_processed, x_train_processed, epochs=maxepoch, batch_size=batch_size, verbose=verbose,
-                                 validation_split=valfrac, callbacks=callbacks)
+                       validation_split=valfrac, callbacks=callbacks)
 
         z_predicted = pd.DataFrame(self.final_encoder.predict(x_train_processed), index=x_train.index)
 
@@ -115,7 +114,7 @@ class autoPLIER:
 
         X_tilde = np.dot(X, pathways.T.to_numpy())
 
-        if(fit == True):
+        if fit is True:
             X_tilde = self.scaler.fit_transform(X_tilde)
             self.n_inputs = X_tilde.shape[1]
             self.build_model()
